401fe162cda45b278abff1d8c1ab2c8f41376b4d
Harry Chopra <harrychopra@gmail.com>
2020-02-06 13:58:24 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,49 +1,22 @@
 var index = require("../index.js")
 
-
-
-describe('Height', ()=>{
-  it('is less than 200', ()=>{
-    expect(index.height).toBeLessThan(200)
-  })
-})
-
-describe('Name', ()=>{
-  it('returns Harry', ()=>{
-    expect(index.name).toEqual("Harry")
-  })
-})
-
-describe('Message',()=>{
-  it('gives the name and height', ()=>{
-    expect(index.message)
-    .toInclude(index.name, index.height)
-  })
-})
-
-
-
-/*
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
+describe("A-Test-Lesson", ()=>{
+  describe('Height', ()=>{
+    it('is less than 200', ()=>{
+      expect(index.height).toBeLessThan(200)
     })
   })
 
-
-  describe('Height', () => {
-    it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
+  describe('Name', ()=>{
+    it('returns Harry', ()=>{
+      expect(index.name).toEqual("Harry")
     })
   })
 
-  describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
+  describe('Message',()=>{
+    it('gives the name and height', ()=>{
+      expect(index.message)
+      .toInclude(index.name, index.height)
     })
   })
 })
-*/
-
