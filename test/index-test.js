0305d18643566075dff6dbb874f160e6fc69296e
Harry Chopra <harrychopra@gmail.com>
2020-02-06 13:54:41 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,28 @@
 var index = require("../index.js")
 
+
+
+describe('Height', ()=>{
+  it('is less than 200', ()=>{
+    expect(index.height).toBeLessThan(200)
+  })
+})
+
+describe('Name', ()=>{
+  it('returns Harry', ()=>{
+    expect(index.name).toEqual("Harry")
+  })
+})
+
+describe('Message',()=>{
+  it('gives the name and height', ()=>{
+    expect(index.message)
+    .toInclude(index.name, index.height)
+  })
+})
+
+
+
 /*
 describe('what-is-a-test', () => {
   describe('Name', () => {
